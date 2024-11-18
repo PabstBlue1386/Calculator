0d8e8b1ce14028c4899f9ded8d57614771df1906
PabstBlue1386 <longer1386@gmail.com>
2024-11-18 19:47:54 +0500

    Add input numbers 1-20

--- src/main/java/ru/pabst/Main.java
+++ src/main/java/ru/pabst/Main.java
@@ -3,6 +3,8 @@ package ru.pabst;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        for (int i = 0; i < 21; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
